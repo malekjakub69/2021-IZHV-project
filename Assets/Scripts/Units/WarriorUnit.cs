53b32a7e2ded3af74db836b3d967ba60594280d0
malekjakub69 <malekjakub69@gmail.com>
2021-12-21 16:39:51 +0100

    Add Unit

---
+++ Assets/Scripts/Units/WarriorUnit.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Units
+{
+    public class WarriorUnit : Unit
+    {
+        protected override void Update()
+        {
+            base.Update();
+        }
+
+        protected override void Move()
+        {
+            if (agent.isOnNavMesh)
+                agent.SetDestination(target);
+        }
+
+        protected override void Fight()
+        {
+            if (agent.isActiveAndEnabled)
+                agent.isStopped = true;
+
+            Rotate(targetedEnemy.transform.position);
+
+            attackTimer -= Time.deltaTime;
+            if (attackTimer < 0)
+            {
+                DealDamage(false);
+                attackTimer = AttackSpeed;
+            }
+        }
+
+        protected override void AttackBase()
+        {
+            if (agent.isActiveAndEnabled)
+                agent.isStopped = true;
+
+            Rotate(enemyBase.transform.position);
+
+            attackTimer -= Time.deltaTime;
+            if (attackTimer < 0)
+            {
+                DealDamage(true);
+                attackTimer = AttackSpeed;
+            }
+        }
+
+        protected override void Die()
+        {
+            //Transfer money
+            Destroy(this.gameObject);
+        }
+
+        private void Rotate(Vector3 targetPosition)
+        {
+            Quaternion rotation = Quaternion.LookRotation(targetPosition);
+            Quaternion.Lerp(transform.rotation, rotation, RotationSpeed);
+        }
+
+        private void DealDamage(bool attackingBase)
+        {
+            if (attackingBase)
+            {
+                //enemyBase.GetComponent <??> ().Health -= AttackDamage;
+            }
+            else
+            {
+                if (targetedEnemy == null)
+                {
+                    target = enemyBase.transform.position;
+                    fighting = false;
+                    agent.isStopped = false;
+                    return;
+                }
+
+                targetedEnemy.GetComponent<Unit>().Health -= AttackDamage;
+                if (targetedEnemy.GetComponent<Unit>().Health <= 0)
+                {
+                    target = enemyBase.transform.position;
+                    fighting = false;
+                    agent.isStopped = false;
+                }
+            }
+        }
+    }
+}
